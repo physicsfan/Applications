0d94f0e7c0e3cffeef99ac639d78150f310ae9cf
Andrew Senchuk <asenchuk@cube.cs.ubc.ca>
2020-01-02 14:15:45 -0800

    update

--- erwf/get_hydrogenic.f90
+++ erwf/get_hydrogenic.f90
@@ -7,7 +7,7 @@
         INTEGER, INTENT(in) :: nrlist
         
         ! local variables
-        INTEGER :: i, j
+        INTEGER :: i, j, k
         REAL(kind=8) :: zeff
         
         WRITE (istdo, '(/,a)') '***** Screening parameters ******' 
@@ -17,6 +17,15 @@
            WRITE (istdo, '(I2,a2,F10.2)') np(i), nh(i), sigma(i)
            zeff = z - sigma(i)                
            CALL DCWF(np(i), nak(i), zeff, e(i), npx, r, p(:,i), q(:,i))
+
+           !   Determine the effective maximum tabulation point
+           k=npx
+           DO
+              IF (ABS(p(k,i)) .GT. 1.d-16) EXIT
+              k = k - 1
+           END DO
+           npt(i) = k
+           
         END DO
         
       END SUBROUTINE get_hydrogenic
