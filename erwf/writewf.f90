6afaecf43060d0c66002d821dcce6c193ea827af
Andrew Senchuk <asenchuk@cube.cs.ubc.ca>
2020-01-02 16:02:03 -0800

    removed .dat file portion

--- erwf/writewf.f90
+++ erwf/writewf.f90
@@ -7,47 +7,26 @@ SUBROUTINE writewf
 !-----------------------------------------------
 !   L o c a l   V a r i a b l e s
 !-----------------------------------------------
-  INTEGER :: i, j, IERR, npxj 
+  INTEGER :: i, j, IERR
 !-----------------------------------------------
 !
 !
-
+  
       OPEN (rwfnout, FILE='rwfn.out', FORM='unformatted', STATUS='replace', iostat=ierr)  
-      OPEN (rwfndat, FILE='rwfn.dat', FORM='formatted', STATUS='replace', iostat=ierr)  
 
-      
       IF (IERR == 1) THEN 
          WRITE (ISTDE, *) 'Error when opening ''rwfn.inp'''
          STOP  
       ENDIF 
-!
-!   Write the file header
-!
-      WRITE (rwfndat,'(a)') 'OOGRWF'
-      WRITE (rwfndat, '(//,a6,2x,i5)') 'npx:', npx
-      WRITE (rwfndat, '(a4,6x,d12.6)') 'h:', h
-
-!      
-!   Write out the radial wavefunctions 
-!
-      DO I = 1, NW 
-         WRITE (rwfndat, '(//,2x,a7,3X,i2,a2,2x,5D12.4)') 'state:', np(i), nh(i)
-         WRITE (rwfndat, '(2x,a7,2x,d12.4)') 'energy:', e(i)
-         WRITE (rwfndat, '(//,a7,8x,a4,i2,a,a,2x,a7,i2,a,a)') 'r','p(',np(i),nh(i),')','q(',np(i),nh(i),')'
-         DO j = 1, npt(i)
-            WRITE (rwfndat, '(d12.4,2x,D12.4,2x,d12.4,2x,i4)') r(j), p(j,i), q(j,i), npt(i)
-         END DO
-      END DO
       
       !   Binary file output
       WRITE (rwfnout) 'G92RWF'
       DO J = 1, NW
          WRITE (rwfnout) NP(J), NAK(J), E(J), npt(j) 
-         WRITE (rwfnout) (P(I,J),I=1,npxj), (Q(I,J),I=1,npxj)
-         WRITE (rwfnout) (R(I),I=1,npxj) 
+         WRITE (rwfnout) (P(I,J),I=1,npt(j)), (Q(I,J),I=1,npt(j))
+         WRITE (rwfnout) (R(I),I=1,npt(j)) 
       END DO 
  
-      CLOSE(rwfndat)
       CLOSE(rwfnout)
 
       RETURN  
